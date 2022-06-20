29328a2c12f86739523e615d4eb042790a7defb4
GHSVS-de <contact-VolliSchlothauer@ghsvs.de>
2022-06-20 15:43:13 +0200

    helper.js. Add findVersionSubRegex + getExists.

--- build/helper.js
+++ build/helper.js
@@ -7,6 +7,7 @@ const util = require("util");
 const rimRaf = util.promisify(require("rimraf"));
 const pc = require('picocolors');
 const crypto = require('crypto');
+const lineReader = util.promisify(require('line-reader').eachLine);
 
 module.exports.cleanOut = async (cleanOuts) =>
 {
@@ -44,7 +45,7 @@ module.exports.getChecksum = async (path, Digest) =>
   });
 }
 
-// Find version string in file. E.g. 'scssphp/scssphp'
+// Find version string in JSON file. E.g. for packageName 'scssphp/scssphp'
 module.exports.findVersionSub = async (packagesFile, packageName) =>
 {
 	console.log(pc.magenta(pc.bold(
@@ -69,9 +70,50 @@ module.exports.findVersionSub = async (packagesFile, packageName) =>
 module.exports.findVersionSubSimple = async (packagesFile, packageName) =>
 {
 	console.log(pc.magenta(pc.bold(
-	`Search versionSub of package "${packageName}" in "${packagesFile}".`)));
+		`Search versionSub of package "${packageName}" in "${packagesFile}".`)));
 
 	return require(packagesFile).version;
+}
+
+// Find version string by RegEx in a file.
+// Example:
+// The string to search in that file(s).
+// const bsBundleRegExp = new RegExp(
+//	/\* Bootstrap \(v(\d+\.\d+\.\d+)\): index\.umd\.js/
+// );
+module.exports.findVersionSubRegex = async (file, thisRegex, limit) =>
+{
+	if (!limit)
+	{
+		limit = 20;
+	}
+
+	console.log(pc.magenta(pc.bold(
+		`Search versionSub. First ${limit} lines of "${file}".`)));
+
+	let foundVersion = '';
+	let count = 0;
+
+	await lineReader(file, function(line)
+	{
+		count++;
+
+		if (count >= limit)
+		{
+			console.log(pc.red(pc.bold(`No version found in first ${count} lines.`)));
+			return false;
+		}
+
+		if (thisRegex.test(line))
+		{
+			foundVersion = line.match(thisRegex)[1];
+			return false;
+		}
+	}).then(function (err) {
+		if (err) throw err;
+	});
+
+	return foundVersion;
 }
 
 module.exports.zip = async (zipOptions) =>
@@ -81,4 +123,15 @@ module.exports.zip = async (zipOptions) =>
 	await zip.writeZip(zipOptions.target);
 	console.log(pc.bgRed(pc.cyan(pc.bold(
 		`ZIP file "${zipOptions.target}" written.`))));
+}
+
+// Check if file or folder exists/is accessible.
+module.exports.getExists = async (path) =>
+{
+  try {
+    await fse.access(path)
+    return true
+  } catch {
+    return false
+  }
 }
